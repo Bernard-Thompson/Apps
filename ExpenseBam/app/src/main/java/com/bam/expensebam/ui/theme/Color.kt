24b53d19ff7a6ba6fa530864a0c1db4d792f7d7d
Bernard-Thompson <thompsonb905@gmail.com>
2023-10-06 22:25:42 -0500

    Added expense tracker application
    Learning more about using kotlin to develop android applications

---
+++ ExpenseBam/app/src/main/java/com/bam/expensebam/ui/theme/Color.kt
@@ -0,0 +1,13 @@
+package com.bam.expensebam.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+
+
+
+val Surface = Color.Black
+val TextPrimary = Color.White
+val BackgroundElevated = Color(0xFF1C1C1E)
+val Primary = Color(0xDD0A84FF)
+val PrimaryVariant = Color(0xFF5E5CE6)
+
